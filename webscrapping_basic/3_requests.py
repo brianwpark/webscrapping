dece473a317838d5489b8c5df70b1a1f3e295275
brianwpark <brian.woojin.park@gmail.com>
2023-08-19 14:43:42 +0000

    added code to show what was requested

--- webscrapping_basic/3_requests.py
+++ webscrapping_basic/3_requests.py
@@ -1,6 +1,13 @@
 import requests as rq
-res = rq.get("http://naver.com")
-print("reply code :", res.status_code) # 200이면 정상
+res = rq.get("http://google.com")
+# res = rq.get("http://nadocoding.tistory.com")
+res.raise_for_status()
+# print("reply code :", res.status_code) # 200이면 정상
 
-if res.status_code == rq.codes.ok:
-    print("Everything is Nominal")
+# if res.status_code == rq.codes.ok:
+#     print("Everything is Nominal")
+# else:
+#     print("ther is a problem. [ErrorCode ", res.status_code, "]")
+
+with open("mygoogle.html", "w", encoding="utf-8") as f:
+    f.write(res.text)
