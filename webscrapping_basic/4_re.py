79b5b6bbe3bf4c12b560c900ab8a77b8a7e71dfd
brianwpark <brian.woojin.park@gmail.com>
2023-08-25 13:26:00 +0000

    created reexp

---
+++ webscrapping_basic/4_re.py
@@ -0,0 +1,13 @@
+import re
+# ca?e
+# care, cafe, case, cave
+
+
+
+p = re.compile("ca.e") 
+# . (ca.e) : 하나의 문자를 의미 > care, cafe, case (O) | caffe (X)
+# ^ (^de) : 문자열의 시작 > desk, destination (O) | fade (X)
+# $ (se$) : 문자열의 끝 > case, base (O) | face (X)
+
+m = p.match("case")
+print(m.group()) # 매치되지 않으면 
