1891de72e2a5deb2df4c049fd1a9c673472984ac
brianwpark <brian.woojin.park@gmail.com>
2023-08-27 14:57:51 +0000

    added basic code for BS4

--- webscrapping_basic/6_bs4.py
+++ webscrapping_basic/6_bs4.py
@@ -1,8 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://comic.naver.com/index"
+url = "https://comic.naver.com/webtoon"
 res = requests.get(url)
 res.raise_for_status()
-
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"}
 soup = BeautifulSoup(res.text, "lxml")
+# print(soup.title)
+# print(soup.title.get_text())
+# print(soup.a) # soup 객체에서 처음 발견되는 a element 출력
+# print(soup.a.attrs) # a element 의 솟성정보를 출력
+# print(soup.a["href"]) # a element 의 href 속성 '값' 정보를 출력
+
+print(soup.find("a", attrs={"class":"SubNavigationBar__link--PXX5B"}))
