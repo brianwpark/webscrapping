dbaef2fe56b6758c823feb2e3a39e0626a7547e0
brianwpark <brian.woojin.park@gmail.com>
2023-09-07 13:24:09 +0000

    Basic Webscrapping using BS4 for Coupang

--- webscrapping_basic/9_bs4_coupang.py
+++ webscrapping_basic/9_bs4_coupang.py
@@ -10,19 +10,42 @@ soup = BeautifulSoup(res.text, "lxml")
 
 items = soup.find_all("li", attrs={"class":re.compile("^search-product")})
 # print(items[0].find("div", attrs={"class":"name"}).get_text())
+
 for item in items:
+
+    # remove all AD
+    ad_badge = item.find("span", attrs={"class":"ad-badge-text"})
+    if ad_badge:
+        print("  <AD is removed>")
+        continue
+
+
     name = item.find("div", attrs={"class":"name"}).get_text() # 제품명
+
+    # remove apple products
+    if "Apple" in name:
+        print("  <Apple product is removed>")
+        continue
+
     price = item.find("strong", attrs={"class":"price-value"}).get_text() # 가격
-    rate = item.find("em", attrs={"class":"rating"}) # 평점
+    
+    # Item with more than 100 review & rating with more than 4.5 will be listed
+    rate = item.find("em", attrs={"class":"rating"}) # rating
     if rate:
         rate = rate.get_text()
     else:
-        rate = "평점 없음"
+        print("  <Item without rating is removed>")
+        continue
     
     rate_cnt = item.find("span", attrs={"class":"rating-total-count"}) # 평점 수
+
     if rate_cnt:
         rate_cnt = rate_cnt.get_text()
+        rate_cnt = rate_cnt[1:-1]
+        # print("number of reviews", rate_cnt)
     else:
-        rate_cnt = "평점 수 없음"
-    
-    print(name, price, rate, rate_cnt)
+        print("  <Item without rating is removed>")
+        continue
+
+    if float(rate) >= 4.5 and int(rate_cnt) >= 100:
+        print(name, price, rate, rate_cnt)
